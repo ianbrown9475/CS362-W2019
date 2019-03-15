3cd826ab6cb24986996538939790804c8f09ade4
ianbrown9475 <ian.brown9475@gmail.com>
2019-03-14 21:02:53 -0700

    Add some test URLs for programming based testing

--- projects/browni/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/browni/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,5 +1,11 @@
 import junit.framework.TestCase;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 // You can use this as a skeleton for your 3 different test approach
 // It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
@@ -23,8 +29,29 @@ public class UrlValidatorTest extends TestCase {
     }
     // You need to create more test cases for your Partitions if you need to
 
-    public void testIsValid() {
-        //You can use this function for programming based testing
+    /**
+     * Programming based testing
+     * @throws FileNotFoundException if problem occurs when loading csv file
+     */
+    public void testIsValid() throws FileNotFoundException {
+        Map<String, Boolean> urlTestCases = new HashMap<>();
+
+        String fileName = "test/urls.csv";
+        Scanner scanner = new Scanner(new File(fileName));
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] fields = line.split(",");
+            urlTestCases.put(fields[0], fields[1].equals("true"));
+        }
+        scanner.close();
+
         UrlValidator urlValidator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        urlTestCases.forEach((key, value) -> {
+            boolean res = urlValidator.isValid(key);
+            assertEquals(value.booleanValue(), res);
+        });
+
+        // Test a null URL
+        assertFalse(urlValidator.isValid(null));
     }
 }
