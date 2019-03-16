7a0ef6a37b50f5ff2afd86e6dfe644eea0c3d4f9
ianbrown9475 <ian.brown9475@gmail.com>
2019-03-16 14:38:30 -0700

    Add remote web page URL parsing

--- projects/browni/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/browni/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,8 +1,14 @@
 import junit.framework.TestCase;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // You can use this as a skeleton for your 3 different test approach
 // It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -30,11 +36,16 @@ public class UrlValidatorTest extends TestCase {
     /**
      * Programming based testing
      *
-     * @throws FileNotFoundException if problem occurs when loading csv file
+     * @throws IOException if problem occurs when loading file or URL
      */
-    public void testIsValid() throws FileNotFoundException {
-        List<List<String>> urlTestCases = new ArrayList<>();
+    public void testIsValid() throws IOException {
+        UrlValidator urlValidator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
+        // Test a null URL
+        assertFalse(urlValidator.isValid(null));
+
+        // Test URLs from test/urls.csv file
+        List<List<String>> urlTestCases = new ArrayList<>();
         String fileName = "test/urls.csv";
         Scanner scanner = new Scanner(new File(fileName));
         while (scanner.hasNext()) {
@@ -44,13 +55,47 @@ public class UrlValidatorTest extends TestCase {
         }
         scanner.close();
 
-        UrlValidator urlValidator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
         urlTestCases.forEach((list) -> {
             boolean res = urlValidator.isValid(list.get(0));
-            assertEquals(list.get(2), list.get(1).equals("true"), res);
+            assertEquals(list.get(2) + ". URL: \"" + list.get(0) + "\"", list.get(1).equals("true"), res);
         });
 
-        // Test a null URL
-        assertFalse(urlValidator.isValid(null));
+        // Test URLs from remote web pages
+        final String[] REMOTE_URLS = {
+                "https://oregonstate.edu",
+                "https://eecs.oregonstate.edu",
+                "https://facebook.com",
+                "https://github.com"
+        };
+        final String URL_REGEX = "(href|src)=\"(http|https|ftp)://[^\" ]+\"";
+
+        for (String remoteUrl : REMOTE_URLS) {
+            Pattern pattern = Pattern.compile(URL_REGEX);
+            URL remoteWebPageUrl = new URL(remoteUrl);
+
+            String remoteWebPageString;
+            try {
+                remoteWebPageString = new Scanner(remoteWebPageUrl.openStream()).useDelimiter("\\A").next();
+            } catch (Exception e) {
+                System.err.println("Could not get URLs from \"" + remoteUrl + "\". Exception: " + e);
+                continue;
+            }
+            Matcher matcher = pattern.matcher(remoteWebPageString);
+
+            List<String> remoteWebPageUrls = new ArrayList<>();
+            while (matcher.find()) {
+                String match = matcher.group();
+                match = match.replaceAll("href=", "");
+                match = match.replaceAll("src=", "");
+                match = match.replaceAll("\"", "");
+                remoteWebPageUrls.add(match);
+            }
+
+            remoteWebPageUrls.forEach(url -> {
+                boolean res = urlValidator.isValid(url);
+                assertTrue("URL from webPage: \"" + url + "\"", res);
+            });
+            System.out.println("Tested " + remoteWebPageUrls.size() + " URLs from remote page: " + remoteUrl);
+        }
     }
 }
